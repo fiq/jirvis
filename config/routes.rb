ad187fae0b576ad16922c03da1ed81ff6ed52943
Raf Gemmail <raf.gemmail@sailthru.com>
2022-08-18 17:00:54 +1200

    versioned API routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api, defaults: {format: 'json'}  do
+    namespace :v1 do
+      resource :projects
+    end
+  end
 end
