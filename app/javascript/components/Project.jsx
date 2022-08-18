5680a538201f430959a2eec35733d89cc542795e
Raf Gemmail <raf.gemmail@sailthru.com>
2022-08-18 19:59:34 +1200

    Added the avatar of the project

--- app/javascript/components/Project.jsx
+++ app/javascript/components/Project.jsx
@@ -4,11 +4,14 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 const lightTheme = createTheme({ palette: { mode: 'light' } });
 
 
 export default (props) => {
     const [projectInfo, setProjectInfo] = React.useState({key:""});
+    const [avatarUrl, setAvatarUrl] = React.useState("");
     const fetchProject = async () => {
         // guard for when props not set
         // TODO simplify
@@ -19,6 +22,7 @@ export default (props) => {
             const body = await response.json();
             setProjectInfo(body);
             console.log(body)
+            setAvatarUrl(body.avatarUrls["48x48"]);
         }
 
         return ()=>console.log("Unmounting project");
@@ -33,11 +37,16 @@ export default (props) => {
             <CardContent>
                 <Typography sx={{ fontSize: 8 }} color="text.secondary" gutterBottom>
                     <Box>
+                    <Stack direction="row" spacing={2}>
+                    <Avatar alt={projectInfo.key} src={avatarUrl}/>    
                     <table>
                         <tr><td>Key:</td><td>{projectInfo.key}</td></tr>
                         <tr><td>Name:</td><td>{projectInfo.name}</td></tr>
                         <tr><td>Description:</td><td>{projectInfo.description}</td></tr>
                     </table>
+                    
+                    
+                    </Stack>
                     </Box>
                 </Typography>
             </CardContent>
